5657330bfab0b3e04ba99fd57a853d53843f4977
Girish S <girish.sonawane@gmail.com>
2013-09-04 11:28:02 +0530

    Passing: spec - converts USD to JPY

--- lib/goog_currency.rb
+++ lib/goog_currency.rb
@@ -10,4 +10,18 @@ module GoogCurrency
     response_hash = JSON.parse(response)
     response_hash['rhs'].to_f
   end
+
+  def self.method_missing(meth, *args)
+    from, to = meth.to_s.split("_to_")
+
+    super(meth, *args) and return if from.nil? or from == "" or to.nil? or to == ""
+
+    response = RestClient.get("http://www.google.com/ig/calculator?hl=en&q=#{args.first}#{from.upcase}=?#{to.upcase}").body
+
+    # response is not valid json
+    response.gsub!(/(lhs|rhs|error|icc)/, '"\1"')
+    response_hash = JSON.parse(response)
+
+    response_hash['rhs'].to_f
+  end
 end
