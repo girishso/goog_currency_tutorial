7174a541e9a31c01051640187b4303cb1687f72f
Girish S <girish.sonawane@gmail.com>
2013-09-04 11:03:33 +0530

    Failing: with error - uninitialized constant GoogCurrency

---
+++ spec/goog_currency_spec.rb
@@ -0,0 +1,20 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'fakeweb'
+
+valid_response =<<-VALID
+  {lhs: "1 U.S. dollar",rhs: "54.836587 Indian rupees",error: "",icc: true}
+VALID
+
+describe "GoogCurrency" do
+  describe "valid currencies" do
+    it "converts USD to INR" do
+      FakeWeb.register_uri(:get,
+                           "http://www.google.com/ig/calculator?hl=en&q=1USD=?INR",
+                           :status => "200",
+                           :body => valid_response)
+      usd = GoogCurrency.usd_to_inr(1)
+      usd.should == 54.836587
+    end
+  end
+end
