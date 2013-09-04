1496e31b1e084dbb77f1533463dc6e8e42559e5f
Girish S <girish.sonawane@gmail.com>
2013-09-04 11:12:14 +0530

    Passing: spec converts USD to INR

--- lib/goog_currency.rb
+++ lib/goog_currency.rb
@@ -1,3 +1,13 @@
-module GoogCurrency
+require "rest_client"
+require "json"
 
+module GoogCurrency
+  def self.usd_to_inr(amount)
+    response = RestClient.get("http://www.google.com/ig/calculator?hl=en&q=#{amount}USD=?INR").body
+
+    # hack to convert API response to valid JSON
+    response.gsub!(/(lhs|rhs|error|icc)/, '"\1"')
+    response_hash = JSON.parse(response)
+    response_hash['rhs'].to_f
+  end
 end
