f05f0b6f2638a5994303be6709e03777ec60df54
Girish S <girish.sonawane@gmail.com>
2013-09-04 11:35:43 +0530

    Failing: spec - throws exception for USD to INX, with error - expected Exception but nothing was raised

--- spec/goog_currency_spec.rb
+++ spec/goog_currency_spec.rb
@@ -10,6 +10,10 @@ valid_response_jpy =<<-VALID
 {lhs: "1 U.S. dollar",rhs: "98.5124618 Japanese yen",error: "",icc: true}
 VALID
 
+invalid_response =<<-INVALID
+{lhs: "",rhs: "",error: "4",icc: false}
+INVALID
+
 describe "GoogCurrency" do
   describe "valid currencies" do
     it "converts USD to INR" do
@@ -28,6 +32,16 @@ describe "GoogCurrency" do
                            :body => valid_response_jpy)
       jpy = GoogCurrency.usd_to_jpy(1)
       jpy.should == 98.5124618
+    end
+  end
+
+  describe "invalid currencies" do
+    it "throws exception for USD to INX" do
+      FakeWeb.register_uri(:get,
+                           "http://www.google.com/ig/calculator?hl=en&q=1USD=?INX",
+                           :status => "200",
+                           :body => invalid_response)
+      expect { GoogCurrency.usd_to_inx(1) }.to raise_error
     end
   end
 end
