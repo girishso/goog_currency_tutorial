382713265f02eeb155baa871106257025ba53be2
Girish S <girish.sonawane@gmail.com>
2013-09-04 11:29:18 +0530

    Passing: after removing usd_to_inr method

--- lib/goog_currency.rb
+++ lib/goog_currency.rb
@@ -2,15 +2,6 @@ require "rest_client"
 require "json"
 
 module GoogCurrency
-  def self.usd_to_inr(amount)
-    response = RestClient.get("http://www.google.com/ig/calculator?hl=en&q=#{amount}USD=?INR").body
-
-    # hack to convert API response to valid JSON
-    response.gsub!(/(lhs|rhs|error|icc)/, '"\1"')
-    response_hash = JSON.parse(response)
-    response_hash['rhs'].to_f
-  end
-
   def self.method_missing(meth, *args)
     from, to = meth.to_s.split("_to_")
 
