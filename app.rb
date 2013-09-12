e9d754f9ce6dc82a0223d7c1f311bc03f1d7e287
Girish S <girish.sonawane@gmail.com>
2013-09-12 21:44:57 +0530

    Passing spec: converts currencies

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
+require File.dirname(__FILE__) + "/lib/goog_currency"
 
 get "/" do
   haml :"index"
+end
+
+post "/convert" do
+  @result = GoogCurrency.send("#{params[:from]}_to_#{params[:to]}".to_sym, params[:amount])
+  haml :"convert"
 end
